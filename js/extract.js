45025b93487f60b2ba2f403e17302dd7b85143d8
Riley Skains <rileyskains@gmail.com>
2018-01-02 13:59:16 -0600

    extract function improvements

--- js/extract.js
+++ js/extract.js
@@ -1,6 +1,11 @@
-function extract() { //Function to rip all six-digit and three-digit hex values.
-    const input = document.getElementById('input').value; //Get value of textarea
-    const regexp = /#([A-F0-9]{6})|#([A-F0-9]{3})/gi
-    const cleansed = input.match(regexp); //JS function to match the hex based off the regular expression.
-    console.log(cleansed);
+function extract() {
+    const input = document.getElementById('input').value;
+    const ripped = input.match(/#([A-F0-9]{6})|#([A-F0-9]{3})/gi);
+    const unique = [...new Set(ripped)];
+    for (const hexValue in unique) {
+        const newElement = document.createElement('div');
+        newElement.id = unique[hexValue]; newElement.className = 'color';
+        newElement.innerHTML = unique[hexValue];
+        document.body.appendChild(newElement);
+    }
     }
