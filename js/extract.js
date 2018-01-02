3071487f07cfc17cb4eb6d672302ff8ad85ebbc3
Riley Skains <rileyskains@gmail.com>
2018-01-02 16:05:13 -0600

    core functionality added

--- js/extract.js
+++ js/extract.js
@@ -1,11 +1,11 @@
 function extract() {
-    const input = document.getElementById('input').value;
+    const input = document.getElementById('input').value.toUpperCase();
     const ripped = input.match(/#([A-F0-9]{6})|#([A-F0-9]{3})/gi);
     const unique = [...new Set(ripped)];
     for (const hexValue in unique) {
         const newElement = document.createElement('div');
-        newElement.id = unique[hexValue]; newElement.className = 'color';
+        newElement.id = unique[hexValue]; newElement.className = 'color'; newElement.style.backgroundColor= unique[hexValue];
         newElement.innerHTML = unique[hexValue];
-        document.body.appendChild(newElement);
-    }
+        document.getElementById('color-container').appendChild(newElement);
+        }
     }
