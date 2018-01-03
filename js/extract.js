754a891bf8d7eac4a42c7e0cd13432b591603057
Riley Skains <rileyskains@gmail.com>
2018-01-02 21:55:55 -0600

    js changes

--- js/extract.js
+++ js/extract.js
@@ -2,7 +2,9 @@ function extract() {
     const input = document.getElementById('input').value.toUpperCase();
     const ripped = input.match(/#([A-F0-9]{6})|#([A-F0-9]{3})/gi);
     const unique = [...new Set(ripped)];
+    const brightness = [];
     for (const hexValue in unique) {
+        brightness.push(tinycolor(unique[hexValue]).getBrightness());
         const newElement = document.createElement('div');
         newElement.id = unique[hexValue];
         newElement.className = 'color'; 
@@ -13,4 +15,6 @@ function extract() {
         newElement.innerHTML = unique[hexValue];
         document.getElementById('color-container').appendChild(newElement);
     }
+    const sorted = brightness.sort((a, b) => b - a);
+    console.log(sorted);
 }
